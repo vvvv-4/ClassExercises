e8a06661d282b766208ab49f4e3efe680733f855
SebnemSK <skuzulugil@gmail.com>
2024-06-14 18:32:48 -0400

    Corrected a typo

--- leapyear.R
+++ leapyear.R
@@ -11,8 +11,9 @@ year <- as.numeric(year)
 # if (year %% 100 == 0) {} 
 
 # option 2 century and ly?
-if(year %% 100 == 0 & year %% 400 == 0){
-# if century AND divisible by 400 -> leapyaer
+if(year %% 400 == 0){
+# if century AND divisible by 400 -> leapyear
+# no need to divide by 100 because it is implicit
   print("yes")
 } else if (year %% 100 != 0 & year %% 4 == 0) {
 # Not a century AND divisible by 4 -> leapyear
