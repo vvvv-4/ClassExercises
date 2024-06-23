8dd3a6aeac047553840e24d2be9beb6806a9cdae
SebnemSK <skuzulugil@gmail.com>
2024-06-23 01:23:12 -0400

    initial commit

---
+++ UDF_use_example.R
@@ -0,0 +1,21 @@
+#' This is a fancy script
+#' that I am writing to
+#' bring world peace
+
+source("helper_functions.R")
+print("We need a magic character to solve hunger")
+magic_char <- user_input_char()
+print("We need another magic character to solve hunger")
+magic_char2 <- user_input_char()
+
+print("We also need a magic number to bring peace")
+
+magic_number <- user_input_num()
+
+print("The magic recipe is as follows:")
+
+for (i in 1:magic_number) {
+  print(magic_char)
+#  print(magic_char2)
+}
+print(magic_char2)
