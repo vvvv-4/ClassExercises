b8d8208b38817c20699dccd732e668c51a63be9f
SebnemSK <skuzulugil@gmail.com>
2024-06-27 17:33:48 -0400

    Inital commit

---
+++ data_wrangling_fns.R
@@ -0,0 +1,19 @@
+# Sample functions for data wrangling
+
+
+today1 <- Sys.Date()
+today2 <- date()
+now <- Sys.time()
+
+# Convert from POSIX to string
+new_now <- strftime(now, "%a, %B %d - %H:%M:%S %y")
+new_date <- strftime(today1, "%a, %B %d - %y")
+
+# Convert from string to POSIX
+new_today <- strptime(today2, "%a %b %d %H:%M:%S %Y")
+
+later <- Sys.time()
+a <- difftime(later, now, units = "mins")
+
+# lubridate example 
+another_now <- lubridate::dmy("17 12, 2012")
