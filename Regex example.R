23800815e1a3270e0d9bb55a5412fca1b39f008a
崔洺溦 <vv@sais-MacBook-Air.local>
2024-06-27 17:58:40 -0400

    no

--- Regex example.R
+++ Regex example.R
@@ -3,9 +3,15 @@
 library(stringr)
 
 sentence <- "This is the first line. \nThis is the second."
-
+phrase <- c("hello", "darkness", "my old", "friend.")
 str_view(sentence, "i")
 #Find all the i in the string "sentence"
 
 str_view(sentence, "[aeiou]")
 #Consider aeiou as a word if no square brackets
+
+str_view(phrase, "[d]$")
+str_view(phrase, ".")
+#Sentence is a vector of one item, while phrase has multiple items. 
+#Period means every characters, to find the literal period, we have to escape it
+str_view(phrase, "\\.")
