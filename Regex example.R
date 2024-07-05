54e7527535c2ef69707ba6d56635185f90ff454e
崔洺溦 <vv@sais-MacBook-Air.local>
2024-07-05 16:31:26 -0400

    yeaa

--- Regex example.R
+++ Regex example.R
@@ -15,3 +15,34 @@ str_view(phrase, ".")
 #Sentence is a vector of one item, while phrase has multiple items. 
 #Period means every characters, to find the literal period, we have to escape it
 str_view(phrase, "\\.")
+str_view(sentence, "\\W")
+#\\W for viewing non-word characters
+
+#Finding phone number in data 
+#[(]?\\d{3}[)]?[|-]?\\d{3}[|-]\\d{4}
+
+#identify the valid emails
+
+emails <- ##Pull from her file
+
+#has the @ sign, excludes item with no @ sign
+str_view(emails,"@")
+
+#Has account before @,anycharacter(+)is used for one or more times
+str_view(emails, ".+@")
+
+#Has account before @, alphanumeric (+) is used for one or more times
+str_view(emails, "[A-Za-z0-9]+@")
+
+#check before n after @ sign for alphanumeric characters
+str_view(emails, "[A-Za-z0-9]+@[A-Za-z0-9]+")
+
+#must contain a domain name and a period after that
+str_view(emails, "[A-Za-z0-9]+@[A-Za-z0-9]+\\.")
+
+#must contain a domain name, a period and a domain extension
+str_view(emails, "[A-Za-z0-9]+@[A-Za-z0-9]+\\.[a-z]{2,4}")
+#Has nothing trailing behind the above
+str_view(emails, "[A-Za-z0-9]+@[A-Za-z0-9]+\\.[a-z]{2,4}\\z")
+
+
---
+++ Tidyverse_examples.R
@@ -0,0 +1,5 @@
+
+library(dplyr)
+
+mydata <- datasets::CO2
+
